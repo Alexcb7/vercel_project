4c53948c28c8554f520c7485c6f6c1592bf10760
Alexcb7 <alex.cortell.ifp@gmail.com>
2025-10-01 10:37:08 +0200

    animated-text

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,38 @@
-import Lightning from './components/Lightning';
+import Lightning from './components/Lightning'
 
+function App() {
+  return (
+    <div style={{ width: '100%', height: '600px', position: 'relative' }}>
+      <Lightning
+        hue={220}
+        xOffset={0}
+        speed={1}
+        intensity={1}
+        size={1}
+      />
+    </div>
+  )
+}
 
-<div style={{ width: '100%', height: '600px', position: 'relative' }}>
+export default App
 
-  <Lightning
+import SplitText from './components/SplitText';
 
-    hue={220}
+const handleAnimationComplete = () => {
+  console.log('All letters have animated!');
+};
 
-    xOffset={0}
-
-    speed={1}
-
-    intensity={1}
-
-    size={1}
-
-  />
-
-</div>
+<SplitText
+  text="Hello, GSAP!"
+  className="text-2xl font-semibold text-center"
+  delay={100}
+  duration={0.6}
+  ease="power3.out"
+  splitType="chars"
+  from={{ opacity: 0, y: 40 }}
+  to={{ opacity: 1, y: 0 }}
+  threshold={0.1}
+  rootMargin="-100px"
+  textAlign="center"
+  onLetterAnimationComplete={handleAnimationComplete}
+/>
